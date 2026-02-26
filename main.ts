8088d6eaefde9182641b07793e84cc52af6cdbc6
lidyaputri-dev <lidya.putri@sma.santaursulajakarta.sch.id>
2026-02-26 14:23:19 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,55 @@
+function Rolldice () {
+    list[randint(0, 5)].showImage(0)
+}
+function setdicepatterns () {
+    list = [
+    images.createImage(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `),
+    images.createImage(`
+        . . . . .
+        . . . # .
+        . . . . .
+        . # . . .
+        . . . . .
+        `),
+    images.createImage(`
+        . . . . .
+        . . . # .
+        . . # . .
+        . # . . .
+        . . . . .
+        `),
+    images.createImage(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # . # .
+        . . . . .
+        `),
+    images.createImage(`
+        . . . . .
+        . # . # .
+        . . # . .
+        . # . # .
+        . . . . .
+        `),
+    images.createImage(`
+        . . . . .
+        . # . # .
+        . # . # .
+        . # . # .
+        . . . . .
+        `)
+    ]
+}
+input.onGesture(Gesture.Shake, function () {
+    Rolldice()
+})
+let list: Image[] = []
+setdicepatterns()
+Rolldice()
